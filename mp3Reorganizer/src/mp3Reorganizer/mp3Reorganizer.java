f9ac77f37096de92891af621f7e2d61341591181
MarianaAthayde <m.athayde@outlook.com>
2017-02-14 23:56:48 -0200

    Alteração para resolver problemas de desempenho

--- mp3Reorganizer/src/mp3Reorganizer/mp3Reorganizer.java
+++ mp3Reorganizer/src/mp3Reorganizer/mp3Reorganizer.java
@@ -1,16 +1,85 @@
 package mp3Reorganizer;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.mp3.Mp3Parser;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 public class mp3Reorganizer {
 
 	public static void main(String[] args) {
-		organizar();		
+		//organizar();		
+		org();
+	}
+	
+	public static void org(){
+		File arquivo = new File(direitorio());
+        File[] file = arquivo.listFiles();
+		List<String> erros = new ArrayList<String>();
+        if (file != null) {
+            int length = file.length;
+            for (int i = 0; i < length; ++i) {
+            	
+            	System.out.println(i + ":" + file[i].getName());
+                if (file[i].isFile() && file[i].getName().substring(file[i].getName().lastIndexOf("."), file[i].getName().length()).equals(".mp3")) {
+                    /*String ARQ = file[i].getPath();
+                    String path = ARQ.replace(file[i].getName(), "");
+                       File fileNovo = new File(path + msc.getNome() + ".mp3");    // Renomeando arquivo ou diretório  */
+                	String nome = null;
+                	
+                	try{
+            			InputStream input = new FileInputStream(new File(file[i].getPath()));
+            			ContentHandler handler = new DefaultHandler();
+            			Metadata metadata = new Metadata();
+            			Parser parser = new Mp3Parser();
+            			ParseContext parseCtx = new ParseContext();
+            			parser.parse(input, handler, metadata, parseCtx);
+            			input.close();	
+            			
+            			nome = metadata.get("title") + "_ " + metadata.get("xmpDM:artist");
+            			
+            		}catch(FileNotFoundException e){
+            			e.printStackTrace();
+            		}catch(IOException e){
+            			e.printStackTrace();
+            		}catch(SAXException e){
+            			e.printStackTrace();
+            		}catch(TikaException e){
+            			e.printStackTrace();
+            		}catch(StringIndexOutOfBoundsException e){
+            			e.printStackTrace();
+            		}
+                	
+                	
+                	String teste = file[i].getPath().replace(file[i].getName(), "");
+                	File fileNovo = new File(teste + nome + ".mp3");
+                    boolean success = file[i].renameTo(fileNovo);
+                    if (!success)
+                    	erros.add(file[i].getName());
+                    
+                }else
+                	erros.add(file[i].getName());                                
+            }
+            if(!erros.isEmpty())
+            	JOptionPane.showMessageDialog(null, join("\n",erros));
+            else
+            	JOptionPane.showMessageDialog(null, "Diretório organizado com sucesso!");
+        } 
 	}
 	
 	public static void organizar(){
